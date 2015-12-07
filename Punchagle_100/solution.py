bac3375f113b009db4caf698b709de9e2bfd850c
James Wang <jameswang9909@hotmail.com>
2015-12-07 16:39:55 -0500

    Solve Punchagle 1

--- Punchagle_100/solution.py
+++ Punchagle_100/solution.py
@@ -1,54 +1,41 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-import itertools
 
 templates = [cv2.imread("img"+str(x)+".png",0) for x in range(10)]
 
 method = cv2.TM_CCORR_NORMED
 
-def round_to_nearest(number, base=5):
-    return int(base * round(float(number)/base))
-
 dec = ""
-#for img in range(1, 10001):
-for i,template in enumerate(templates):
-    data = {}
-    #img_rgb = cv2.imread('images/img%s.png' % img)
-    img_rgb = cv2.imread('example1.png')
-    #print "img%s template%s" % (img,i)
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    w, h = template.shape[::-1]
-    res = cv2.matchTemplate(img_gray, template, method)
-    threshold = .98
-    loc = np.where( res >= threshold)
-    for pt in zip(*loc[::-1]):
-        #i think this x,y shit is wrong. doesn't match up with Pinta.
-        #flipped and complemented or something
-        x = round_to_nearest(pt[0])
-        y = round_to_nearest(pt[1])
-        if (x,y) in data:
-            assert False, "something wrong here"
-        data[(x,y)] = i
-        # print y, pt, i
-        # cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    #make assoc array
-    data = zip(data.keys(), data.values())
-    #sort by y
-    print i
-    # y1 > y2: y1 above
-    # x1 > x2: x1 on left
-    def compare(a,b):
-        return (b[0][1] - a[0][1]) * 10000 + (b[0][0] - a[0][0])
-    for p in sorted(data, cmp=compare):
-        print p
+for img in range(1, 10001):
+    base = 2
+    shapes = []
+    for i,template in enumerate(templates):
+        img_rgb = cv2.imread('images/img%s.png' % img)
+        print "img%s template%s" % (img,i)
+        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+        w, h = template.shape[::-1]
+        res = cv2.matchTemplate(img_gray, template, method)
+        threshold = .98
+        loc = np.where( res >= threshold)
+        for pt in zip(*loc[::-1]):
+            x = pt[0]
+            y = pt[1]
+            shapes.append((x, y, i))
 
-    # cv2.imwrite('res'+str(i)+'.png', img_rgb)
-# for row in data:
-#     for x in range(1,4):
-#         for candidate in itertools.product(data[row], repeat=x):
-#             try:
-#                 char = "".join(str(x) for x in candidate)
-#                 dec += char
-#             except:
-#                 pass
+    shapes = sorted(shapes, key=lambda x: x[1])
+
+    while base <= len(shapes):
+        row = sorted(shapes[:base])
+        dec += "".join([str(x[2]) for x in row])
+        shapes = shapes[base:]
+        base += 1
+
+index = dec.find("11599116102")
+if index != -1:
+    print "The flag is probably somewhere in this string: %s" % dec[index:index+100]
+else:
+    print "Could not find the flag :("
+
+# Decode the outputted string into corresponding ascii values
+
+# sctf{prettycoolrite}
