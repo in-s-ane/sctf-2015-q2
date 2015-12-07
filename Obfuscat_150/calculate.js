f05b3c17d1b06f95575d44b821dcd7eb155129a7
James Wang <jameswang9909@hotmail.com>
2015-12-06 22:15:19 -0500

    Clean up solution to Obfuscat

--- Obfuscat_150/calculate.js
+++ Obfuscat_150/calculate.js
@@ -1,11 +1,11 @@
 var x = 0;
-var seed = function() {
+var calculate_seed = function() {
     for (;;) {
         var m = new MersenneTwister(x);
         if (((m.genrand_int32() * Math.sqrt(m.random())) & 255) == 198) {
             if (((m.genrand_int32() * Math.sqrt(m.random())) & 255) == 58) {
                 if (((m.genrand_int32() * Math.sqrt(m.random())) & 255) == 110) {
-                    alert("Seed " + x + " has passed three tests. This is probably the right seed");
+                    console.log("Seed " + x + " has passed three tests. This is probably the right seed");
                     break;
                 }
             }
@@ -14,7 +14,7 @@ var seed = function() {
     }
 }
 
-var idk = function() {
+var query2_0 = function() {
     var m = new MersenneTwister(4744);
     // Call three times to get to the random we want
     m.genrand_int32();
@@ -23,16 +23,32 @@ var idk = function() {
     m.random();
     m.genrand_int32();
     m.random();
-    alert((m.genrand_int32() * Math.sqrt(m.random())) & 255);
+    console.log(String.fromCharCode(((m.genrand_int32() * Math.sqrt(m.random())) & 255) ^ 49));
 }
 
-var wat = function() {
+var query2_6 = function() {
     var i = 0;
     for (;;) {
+        // -821768299 is h.words.splice(0, 1) when it gets evaluated
         if (String.fromCharCode(i ^ -821768299 & 255) == "s") {
-            console.log(i);
+            console.log(String.fromCharCode(i/2));
             break;
         }
         i++;
+    }
+}
+
+var query2_8 = function() {
+    function isInt(n){
+        return Number(n) === n && n % 1 === 0;
+    }
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for(var i=0; i<alphabet.length; i++) {
+        var chr = alphabet.charAt(i);
+        var num = Math.pow((3111809 - chr.charCodeAt(0)), .25);
+        if (isInt(num)) {
+            console.log(chr);
+            console.log(String.fromCharCode(num ^ 95))
+        }
     }
 }
--- Obfuscat_150/deobfuscated.js
+++ Obfuscat_150/deobfuscated.js
@@ -5,20 +5,17 @@ document.getElementById("panel").onclick = function() {
     setTimeout(function() {
         try {
             console.log(location.search.length);
-            if (location.search.length != 25) throw Error();
-            console.log("Checkpoint 1");
+            if (location.search.length != 25) throw Error(); // Search length of 25, including the ?
             var query = location.search.substring(1).split(/{|}/); // Split by { or }
             if (query[2]) throw Error();
-            console.log("Checkpoint 2");
             var message = ("The " + query[0] + " is a string.").split(" ");
-            var query2 = query[1].split("$");
-            // Check calculate.py for code that calculates this
+            var query2 = query[1].split("$"); // Split by $
+            // Run query2_8 in calculate.js for query2[8]
             if (query2[8].split("").map(function(k) {
                 return k.charCodeAt(0);
             }).reduce(function(y, n) {
                 return y + Math.pow(n ^ 95, 4);
             }) != 3111809) throw Error();
-            console.log("Checkpoint 3");
             for (var i in message) {
                 if (i != false) {
                     var q = message.splice(i, 1).join("");
@@ -37,6 +34,9 @@ document.getElementById("panel").onclick = function() {
             // We want query2[4] to yield "t", 156 to yield "h", and so on.
             // Again, since the first element will be the base of the string,
             // query2[4] must be "t"
+            // Looking at query2[6], we can tell that it runs function c on the character code of query2[6][0] * 2/
+            // Run query2_6 from calculate.js
+            // query2[6] must be "s"
             function(c) {
                 return [query2[4], 156, 230, (query2[6].charCodeAt(0) * 2)].reduce(c);
             }];
@@ -77,45 +77,34 @@ document.getElementById("panel").onclick = function() {
                 // This calls funcs[3] using the next function as its parameter.
                 // Since m is the base, the first element must be "t"
                 funcs[query2[8].charCodeAt(1) - 114](function(m, n) {
-                    console.log(m);
-                    console.log(h.words);
                     return m + (funcs[0](n ^ h.words.splice(0, 1) & 255));
                     }),
 
                 // This must yield "str1ng"
                 // ((m.genrand_int32() * Math.sqrt(m.random())) & 255) ^ x on each element in the array must yield "str1ng" when joined together
-                // We can set the seed manually up above, just gotta figure out what seed we want...
-                // Run calculate.js to calculate the seed we need
-                // It is highly likely that the seed is 4744.
-                //
-                // var m = new MersenneTwister((funcs[0] + "" [(query2[0] / 21)]).split(/\s/).join("").split("").map(function(x) {
-                //     return x.charCodeAt(0);
-                // }).reduce(add) + 160);
-                //
-                // This means that the total sum of the char codes for "function fromCharCode() { [native code] }" must yield 4584
-                // Apparently I'm also dumb because the above code evaluates to this...
-                // Anyways, now with that out of the way, we need to figure out what query2[0] is.
-                // Let's use the random thing again and get what we need to make "1".
-                // running idk() from calculate.js, query2[0] is 'b'
+                // The seed is 4744 btw.
+                // Let's run our random functions again 3 times so that we get the random we want to calculate query2[0]
+                // Running query2_0() from calculate.js, query2[0] is 'S'
                 [181, 78, 28, query2[0].charCodeAt(0), 225, 129].map(function(x) {
                     return funcs[0](((m.genrand_int32() * Math.sqrt(m.random())) & 255) ^ x);
                     }).join("")
             ].join(funcs[0](query2[3].charCodeAt(0) + 15));
             console.log(string);
             // if (CryptoJS.MD5(string).toString() == "0f957300a52431c2d0de0da9fc7223c9") {
+            // Looking up the hash on http://md5online.net, it reverses to r3v3rse_th1s_str1ng
             if (string == "r3v3rse_th1s_str1ng") {
+                // Correct!
                 document.getElementById("message").innerHTML = "Correct!";
             } else {
-                throw Error();
+                // Wrong
             }
         } catch (e) {
+            // Wrong
             console.log(e.stack);
-            document.getElementById("message").innerHTML = "Nope. :(";
         }
         }, 1000);
     };
 
 // Try reading my notes on this if you can.
-// tl;dr, this was really annoying
-//
-// sctf{S$z$e$P$t$@$s$r$qu}
+
+// flag{S$z$e$P$t$@$s$r$qu}
